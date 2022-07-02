86acfeaa615539cab778f6727757f487faa5ba18
Joma336 <joma.mo1381@gmail.com>
2022-07-02 11:29:32 +0430

    I added a lot of class

--- src/Friend.java
+++
@@ -1,9 +0,0 @@
-public class Friend extends Person {
-    public Friend(String userName, String email) {
-        super(userName, email);
-    }
-
-    public Friend(String userName, String email, String phoneNumber) {
-        super(userName, email, phoneNumber);
-    }
-}
